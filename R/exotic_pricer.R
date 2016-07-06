6f36d3d728cdfb76504055efbaf4ffff1a569842
Ivan Zoccolan <ivan.zoccolan@gmail.com>
2016-07-06 15:45:12 +0000

    Started developing the exotic engine and related tests. Changed cash_flow to cash_flows since we want to leverage R vectorization and avoid for loops.

--- R/exotic_pricer.R
+++ R/exotic_pricer.R
@@ -2,52 +2,52 @@
 #Porting of C++ code by M. Joshi from C++ Design Patterns and Derivatives Pricing
 
 
-#' Class providing a cash flow object with methods to get/set the cash flow amount.
+#' Class providing a cash flow object with methods to get/set the cash flow amounts.
 #'
 #' @docType class
 #' @importFrom R6 R6Class
 #' @export
-#' @return Object of \code{\link{R6Class}} with methods to get/set the cash flow amount.
+#' @return Object of \code{\link{R6Class}} with methods to get/set the cash flow amounts.
 #' @format \code{\link{R6Class}} object.
 #' @examples
 #' tt <- timeDate::Sys.timeDate()
-#' cash <- cash_flow$new(tt, 50.89)
-#' cash$get_amount()
-#' cash$set_amount(60.00)
-#' cash$get_amount()
-#' @field amount \code{numeric} Stores the amount of the cash flow.
-#' @field time_index \code{\link{timeDate}} Stores the time index of the cash flow.
+#' cash <- cash_flows$new(tt, 50.89)
+#' cash$get_amounts()
+#' cash$set_amounts(60.00)
+#' cash$get_amounts()
+#' @field amounts \code{numeric} Stores the amounts of the cash flows.
+#' @field time_index \code{\link{timeDate}} Stores the time-line of the cash flows.
 #' @section Methods:
 #' \describe{
-#'   \item{\code{get_amount}}{get method for the amount of the cash flow}
+#'   \item{\code{get_amounts}}{get method for the amounts of the cash flow. Returns a \code{numeric} scalar or vector}
 #'
-#'   \item{\code{set_amount}}{set method for the amount of the cash flow }
+#'   \item{\code{set_amounts}}{set method for the cash flow amounts }
 #'   }
 
 
 
-cash_flow <- R6::R6Class("cash_flow", public = list(
-  get_amount = function(){private$amount},
-  set_amount = function(the_amount){
-    if(!missing(the_amount))
-      if (is_not_negative_scalar(the_amount)) private$amount <- the_amount
-      else stop("argument must be a non-negative scalar\n")
-    else private$amount <- 0
+cash_flows <- R6::R6Class("cash_flows", public = list(
+  get_amounts = function(){private$amounts},
+  set_amounts = function(the_amounts){
+    if(!missing(the_amounts))
+      if (inherits(the_amounts, "numeric")) private$amounts <- the_amounts
+      else stop("argument must be numeric\n")
+    else private$amounts <- numeric(0)
   },
-  initialize = function(time, the_amount){
+  initialize = function(time, the_amounts){
     if (!missing(time)){
       if (inherits(time, "timeDate")) private$time_index <- time
       else stop("argument must be a timeDate object\n")
     } else private$time_index <- timeDate::Sys.timeDate()
 
-    if(!missing(the_amount)){
-      if (inherits(the_amount, "numeric")) private$amount <- the_amount
+    if(!missing(the_amounts)){
+      if (inherits(the_amounts, "numeric")) private$amounts <- the_amounts
       else stop("argument must be numeric\n")
-    } else private$amount <- 0.
+    } else private$amounts <- 0
   }
   ),
  private = list(
-  amount = "numeric",
+  amounts = "numeric",
   time_index = "timeDate"
  )
 )
@@ -55,7 +55,7 @@ cash_flow <- R6::R6Class("cash_flow", public = list(
 
 #' Class which defines  the interface for a  path dependent derivative product
 #' @description Base class which defines the interface for a path dependent derivative
-#' product. This class should be imported by subclasses implementing its methods.
+#' product. This class should be inherited by subclasses implementing its methods.
 #' Objects of this class should not be instantiated.
 #' @docType class
 #' @importFrom R6 R6Class
@@ -64,13 +64,14 @@ cash_flow <- R6::R6Class("cash_flow", public = list(
 #' @export
 #' @return Object of \code{\link{R6Class}}
 #' @format \code{\link{R6Class}} object.
-#' @field time_index A \code{\link{timeDate}} object storing the cash flow times .
+#' @field time_index A \code{\link{timeDate}} object storing the product time-line .
 #' @section Methods:
 #' \describe{
-#'   \item{\code{get_times}}{get method for the cash flow times}
-#'   \item{\code{max_number_cfs}}{returns the maximun number of cash flows the product can generate}
-#'   \item{\code{cash_flow_times}}{retuns the possible cash flow times of the product}
-#'   \item{\code{cash_flows}}{returns the cash flows of the product}
+#'   \item{\code{new}}{Constructor method which takes as argument a \code{\link{timeDate}} object with the product time-line. If no argument is specified it gets initialized with a year long daily sequence}
+#'   \item{\code{get_times}}{get method for the product time-line. Retuns a \code{\link{timeDate}} object}
+#'   \item{\code{max_number_cfs}}{ returns an \code{integer} with the maximun number of cash flows the product can generate}
+#'   \item{\code{cash_flow_times}}{retuns a \code{\link{timeDate}} object with the possible cash flow times. Within this base class the method simply returns the product time-line.}
+#'   \item{\code{cash_flows}}{returns a \code{\link{cash_flows}} object with the cash flows of the product}
 #'   }
 
 path_dependent <- R6::R6Class("path_dependent", public = list(
@@ -78,13 +79,50 @@ path_dependent <- R6::R6Class("path_dependent", public = list(
       if (!missing(the_times))
         if (inherits(the_times, "timeDate")) private$times <- the_times
         else stop("argument must be a timeDate object\n")
-      else private$times <- timeDate::Sys.timeDate()
+      else private$times <- timeDate::timeSequence("2016-01-01", "2016-12-31")
     },
-    get_times = function(){private$times},
-    max_number_cfs = function(){},
-    cash_flow_times = function(){},
-    cash_flows = function(spot_values, generated_flows){}),
+    get_times = function() private$times,
+    max_number_cfs = function() length(private$times),
+    cash_flow_times = function() private$times,
+    cash_flows = function(spot_values){
+      tt  <- self$cash_flow_times()
+      cash_flows$new(tt, numeric(length(tt)))
+    }
+    ),
   private = list(
     times = "timeDate"
+  )
+)
+
+
+exotic_engine <- R6Class("exotic_engine",
+  public = list(
+    initialize = function(product, interest){
+      if(!missing(product))
+        if (inherits(product, "path_dependent")) {
+          private$the_product <- product
+          private$these_cash_flows <- product$cash_flows()
+        } else stop("argument must be a path_dependent object")
+      else stop("argument must be a path_dependent object")
+
+      if(!missing(interest))
+        if(inherits(interest, "parameters")) {
+          private$r <- interest
+          cf_times <- product$cash_flow_times()
+          t0 <- cf_times[1]
+          log_discounts <- sapply(cf_times, function(t) -interest$integral(t0, t))
+          private$discounts <- exp(log_discounts)
+        } else stop("argument must be a parameters object")
+      else stop("argument must be a parameters object")
+    },
+    get_one_path = function(){},
+    do_simulation = function(the_gatherer, number_of_paths){},
+    do_one_path = function(){}
+  ),
+  private = list(
+    the_product = "path_dependent",
+    r = "parameters",
+    discounts = "numeric",
+    these_cash_flows = "cash_flows"
   )
 )
--- tests/testthat/test_cash_flow.R
+++ tests/testthat/test_cash_flow.R
@@ -1,18 +1,24 @@
-context("tests with cash_flow class")
+context("tests with cash_flows class")
 
 test_that("initialize works as expected", {
-  expect_is(cash_flow$new(timeDate::Sys.timeDate(), 67.89), "R6")
-  expect_error(cash_flow$new("pippo"), "argument must be a timeDate object")
-  expect_error(cash_flow$new(timeDate::Sys.timeDate(), "pippo"), "argument must be numeric")
+  expect_is(cash_flows$new(timeDate::Sys.timeDate(), 67.89), "R6")
+
+  tt <- timeDate::timeSequence("2016-01-01", "2016-12-31")
+  amounts <- seq_along(tt) / 100
+  expect_is(cash_flows$new(tt,amounts), "R6")
+
+  expect_error(cash_flows$new("pippo"), "argument must be a timeDate object")
+  expect_error(cash_flows$new(timeDate::Sys.timeDate(), "pippo"), "argument must be numeric")
+
   }
 )
 
 test_that("get and set methods work as expected", {
-  cash <- cash_flow$new(timeDate::Sys.timeDate(),45.00)
-  expect_equal(cash$get_amount(), 45)
-  cash$set_amount(50.67)
-  expect_equal(cash$get_amount(), 50.67)
-  expect_error(cash$set_amount("pippo"), "argument must be a non-negative scalar")
+  cash <- cash_flows$new(timeDate::Sys.timeDate(),45.00)
+  expect_equal(cash$get_amounts(), 45)
+  cash$set_amounts(50.67)
+  expect_equal(cash$get_amounts(), 50.67)
+  expect_error(cash$set_amounts("pippo"), "argument must be numeric")
 
 })
 
